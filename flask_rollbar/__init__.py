777b376354fde0cdbe8a69d9a79ce13803ac311a
Matt Smith <msmith@riotgames.com>
2016-01-11 10:16:15 +0000

    incr version #

--- flask_rollbar/__init__.py
+++ flask_rollbar/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Matt Smith <matt.daemon660@gmail.com>'
 from .flask_rollbar import (
     Rollbar,
